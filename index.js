9dc5d8f2f445e68093aaade60d1b23f64cf32322
blueglass <cardanoviette123@gmail.com>
2022-08-17 17:43:46 -0500

    Update index with first API endpoint

--- index.js
+++ index.js
@@ -21,11 +21,27 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/", function(req,res) {
+  let date = new Date();
+  res.json({
+    "unix": date.valueOf(),
+    "utc": date.toUTCString()
+  });
+})
+app.get("/api/:timestamp", function(req, res) {
+  let dateString = req.params.timestamp;
+  if(!/-/.test(dateString)) dateString = +dateString; // in case is in unix form
+
+  let date = new Date(dateString);
+  if( date === "Invalid Date"){
+    res.json({error: "Invalid Date"});
+  }else{
+    res.json({
+      "unix": date.valueOf(),
+      "utc": date.toUTCString()
+    });
+  }
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(port, function () {
